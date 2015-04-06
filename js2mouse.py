caa628f9971d79916da22cf6b4dc8c06238eb9f3
arty <art.yerkes@gmail.com>
2015-04-05 22:26:27 -0700

    Rate limit wrapper for a stream.

--- js2mouse.py
+++ js2mouse.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from pygame.locals import *
 import pygame, pygame.joystick, pygame.event
 
@@ -11,9 +12,11 @@ class GamePadInput:
     def __init__(self):
         self.stick = None
         self.sticks = dict([(s.get_id(),s) for s in [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]])
+        self.events = []
         self.hats = None
         self.axes = None
         self.buttons = None
+        self.serial = 0
 
     def attach(self,sid):
         self.stick = self.sticks[sid]
@@ -31,26 +34,59 @@ class GamePadInput:
     def refresh_buttons(self):
         self.buttons = [self.stick.get_button(x) for x in range(self.stick.get_numbuttons())]
 
-    def run(self):
-        while True:
-            for event in pygame.event.get():
-                print 'event',event.type
-                if event.type == QUIT:
-                    print 'quit event'
-                    return
-                elif event.type == JOYAXISMOTION:
-                    self.refresh_axes()
-                    print 'axes',self.axes
-                elif event.type == JOYBUTTONUP or event.type == JOYBUTTONDOWN:
-                    self.refresh_buttons()
-                    print 'buttons',self.buttons
-                elif event.type == JOYHATMOTION:
-                    self.refresh_hats()
-                    print 'hats',self.hats
+    def stream(self):
+        if self.events is None:
+            return None
+        self.events = pygame.event.get()
+        for event in self.events:
+            self.serial += 1
+            if event.type == QUIT:
+                self.events = None
+                break
+            elif event.type == JOYAXISMOTION:
+                self.refresh_axes()
+            elif event.type == JOYBUTTONUP or event.type == JOYBUTTONDOWN:
+                self.refresh_buttons()
+            elif event.type == JOYHATMOTION:
+                self.refresh_hats()
+        self.events = []
+        return {"serial":self.serial, "axes":self.axes, "buttons":self.buttons, "hats":self.hats}
+
+class rate_limiter:
+    def __init__(self,fun,pred,timings):
+        self.fun = fun
+        self.pred = pred
+        self.timings = timings
+        self.mode = 0
+        self.last = None
+        self.reset_remaining()
+
+    def reset_remaining(self):
+        self.remaining = self.timings[self.mode][0]
     
+    def stream(self):
+        sleeptime = self.timings[self.mode][1]
+        print 'sleep',sleeptime
+        time.sleep(sleeptime)
+        event = self.fun()
+        if self.last is None or self.pred(self.last,event):
+            if self.remaining is not None:
+                print 'remaining',self.remaining
+                self.remaining -= 1
+                if self.remaining == 0:
+                    self.mode += 1
+                    self.reset_remaining()
+        else:
+            self.mode = 0
+            self.reset_remaining()
+        self.last = event
+        return event
+
 job = GamePadInput()
 if len(sys.argv) > 1:
     job.attach(int(sys.argv[1]))
-    job.run()
+    r = rate_limiter(job.stream,lambda x,y: x['serial'] == y['serial'],[(250,0.01),(250,0.1),(None,1)])
+    for event in iter(r.stream, None):
+        print event
 else:
     print job.sticks
